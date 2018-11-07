60440b0fef55a4396357361e65b324ac709ba722
jeongseonpark <sun001126@hanmail.net>
2018-11-07 16:52:10 +0900

    2018-11-07 첫깃헙2

---
+++ page/working_car.php
@@ -0,0 +1,25 @@
+<?php
+include_once("../common.php");
+
+include_once(G5_PATH."/head2.php");
+
+?>
+
+    <div class="contents">
+        <div id="ctn_wrap">
+            <div class="ctn_tit">
+                <h2><span>일할때</span>타는차</h2>
+                <p>A CAR THAT RIDES WHEN WORKING</p>
+            </div>
+            <div class="ctn_txt ctn_img">
+                <img src="<?php echo G5_IMG_URL ?>/all_car.jpg" alt="전체 자동차 리스트">
+            </div>
+        </div>
+    </div>
+
+
+
+
+<?php
+include_once(G5_PATH."/tail.php");
+?>
